1c7728e363acc3b0f18a96bca4becbc052de410d
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2021-10-06 12:50:44 -0300

    complementando o exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/lista-de-exercicios3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/lista-de-exercicios3.js
@@ -1,10 +1,10 @@
 // 1 - Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
-function quadrado(n){
+function quadrado(n) {
     let quadrado = [];
-    for(let i = 0; i<n; i++){
+    for (let i = 0; i < n; i++) {
         let linha = [];
-        for(let j = 0; j<n; j++){
+        for (let j = 0; j < n; j++) {
             linha.push("*");
         }
         linha = linha.join("");
@@ -18,11 +18,11 @@ console.log(quadrado(4))
 
 // 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
-function triangulo(n){
+function triangulo(n) {
     let quadrado = [];
-    for(let i = 0; i<=n; i++){
+    for (let i = 0; i <= n; i++) {
         let linha = [];
-        for(let j = 0; j<i; j++){
+        for (let j = 0; j < i; j++) {
             linha.push("*");
         }
         linha = linha.join("");
@@ -37,14 +37,14 @@ console.log(triangulo(4))
 // 3- Agora inverta o lado do triângulo. Por exemplo:
 // Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
 
-function triangulo(n){
+function triangulo(n) {
     let quadrado = [];
-    for(let i = 0; i<=n; i++){
+    for (let i = 0; i <= n; i++) {
         let linha = [];
-        for(let j = 0; j<n; j++){
-            if(j<i){
+        for (let j = 0; j < n; j++) {
+            if (j < i) {
                 linha.push(" ");
-            } else if (j>=i) {
+            } else if (j >= i) {
                 linha.push("*");
             }
         }
@@ -59,13 +59,12 @@ function triangulo(n){
 console.log(triangulo(4));
 
 // 4- Depois, faça uma pirâmide com n asteriscos de base:
-// 1 - Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
-function quadrado(n){
+function quadrado(n) {
     let quadrado = [];
-    for(let i = 0; i<n; i++){
+    for (let i = 0; i < n; i++) {
         let linha = [];
-        for(let j = 0; j<n; j++){
+        for (let j = 0; j < n; j++) {
             linha.push("*");
         }
         linha = linha.join("");
@@ -79,11 +78,11 @@ console.log(quadrado(4))
 
 // 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
-function triangulo(n){
+function triangulo(n) {
     let quadrado = [];
-    for(let i = 0; i<=n; i++){
+    for (let i = 0; i <= n; i++) {
         let linha = [];
-        for(let j = 0; j<i; j++){
+        for (let j = 0; j < i; j++) {
             linha.push("*");
         }
         linha = linha.join("");
@@ -98,14 +97,14 @@ console.log(triangulo(4))
 // 3- Agora inverta o lado do triângulo. Por exemplo:
 // Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
 
-function trianguloInvertido(n){
+function trianguloInvertido(n) {
     let quadrado = [];
-    for(let i = 0; i<=n; i++){
+    for (let i = 0; i <= n; i++) {
         let linha = [];
-        for(let j = 0; j<n; j++){
-            if(j<i){
+        for (let j = 0; j < n; j++) {
+            if (j < i) {
                 linha.push(" ");
-            } else if (j>=i) {
+            } else if (j >= i) {
                 linha.push("*");
             }
         }
@@ -121,24 +120,24 @@ console.log(trianguloInvertido(4));
 
 // 4- Depois, faça uma pirâmide com n asteriscos de base:
 
-function piramide(n){
+function piramide(n) {
     let quadrado = [];
-    for(let i = 0; i<=n; i++){
+    for (let i = 0; i <= n; i++) {
         let linha = [];
-        for(let j = 0; j<n; j++){
-            if(j<i){
+        for (let j = 0; j < n; j++) {
+            if (j < i) {
                 linha.push(" ");
-            } else if (j>=i) {
+            } else if (j >= i) {
                 linha.push("*");
             }
         }
         linha = linha.join("");
-        n = Math.round(n/2)+1
+        n = Math.round(n / 2) + 1
         quadrado.push(linha);
         linha = [];
     }
     quadrado.reverse();
-    return quadrado.join("\n");   
+    return quadrado.join("\n");
 }
 
 console.log(piramide(5))
@@ -147,20 +146,20 @@ console.log(piramide(5))
 // Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
 // Dica: você vai precisar fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
 
-function verificaPrimo(n){
+function verificaPrimo(n) {
     let cont = 0;
-    if(n<=1){
+    if (n <= 1) {
         return `${n} não é primo`;
     }
-    for(let i = 2; i<n; i++){
-        if(n%i===0){
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
             cont++;
         }
     }
-    if(cont>0){
+    if (cont > 0) {
         return `${n} não é primo`;
     }
-    else if (cont===0){
+    else if (cont === 0) {
         return `${n} é primo`;
     }
 }
