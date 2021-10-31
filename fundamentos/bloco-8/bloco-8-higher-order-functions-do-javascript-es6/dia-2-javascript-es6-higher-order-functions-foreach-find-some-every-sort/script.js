a50f1f88a0e43b2ee5162035e707a2e91df6b545
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2021-10-31 16:00:16 -0300

    exercise 1 and 2

--- fundamentos/bloco-8/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ fundamentos/bloco-8/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -138,3 +138,35 @@ function authorUnique() {
   books.forEach((value) => storeBirthYears.push(value.author.birthYear));
   return new Set(storeBirthYears).size === storeBirthYears.length;
 }
+
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+function formatedBookNames() {
+  let infoBooks = [];
+  books.map((value) =>
+    infoBooks.push(`${value.name} - ${value.genre} - ${value.author.name}`)
+  );
+  return infoBooks;
+}
+
+console.log(formatedBookNames());
+
+//2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+function objNameAndAge(el) {
+  let obj = {
+    name: "",
+    age: 0,
+  };
+  obj.name = el.author.name;
+  obj.age = el.releaseYear - el.author.birthYear;
+  return obj;
+}
+
+function nameAndAge() {
+  let storeBookInfo = [];
+  books.map((value) => storeBookInfo.push(objNameAndAge(value)));
+  return storeBookInfo.sort((a, b) => a.age - b.age);
+}
+
+console.log(nameAndAge());
