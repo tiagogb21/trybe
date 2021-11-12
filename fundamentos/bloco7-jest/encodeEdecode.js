0ca6d5fa53d13974b21283b4b6b78d9c76975299
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2021-11-12 20:18:58 -0300

    jest 2

--- fundamentos/bloco7-jest/encodeEdecode.js
+++ fundamentos/bloco7-jest/encodeEdecode.js
@@ -1,5 +1,16 @@
 function encode(str){
   let vogals = 'aeiou';
-  let output = ''
-  return str.split('').map((value)=>if(vogals.indexOf(value)>-1?output+=value:output+=(vogals.indexOf(value)+1)))
+  let output = '';
+  str.split('').map((value)=>vogals.indexOf(value)>-1?output+=(vogals.indexOf(value)+1):output+=value);
+  return output
 }
+
+function decode(str){
+  let vogals = 'aeiou';
+  let numbers = '12345';
+  let output = '';
+  str.split('').map((value)=>numbers.indexOf(value)>-1?output+=vogals[numbers.indexOf(value)]:output+=value);
+  return output
+}
+
+module.exports = {encode,decode};
---
+++ fundamentos/bloco7-jest/encodeEdecode.test.js
@@ -0,0 +1,1 @@
+const encodeEdecode = require('./encodeEdecode.js');
